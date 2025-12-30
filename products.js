78411ee9e5a8f731211c26e88fe2e8fde707db86
hamid butt <hamid2007butt@gmail.com>
2025-12-30 22:12:50 +0500

    Separating common functions btw pages, added quantity btn

--- products.js
+++ products.js
@@ -1,12 +1,15 @@
 const products = [
-    {name : "Face Wash", companyName : "ORGATRE", category : "beauty",src : "assets/IMAGES/Beauty/antiaging Face wash.png",off: 10,price : 1299, des : "lorem1234"},
-    {name : "Body Lotion", companyName : "eos", category : "beauty",src : "assets/IMAGES/Beauty/Body lotion.png",off: 20, price : 449, des : "lorem1234"},
-    {name : "Body Scrub", companyName : "Dove", category : "beauty",src : "assets/IMAGES/Beauty/Body Scrub.png",off: 10, price : 599, des : "lorem1234"},
-    {name : "Eyeshadow", companyName : "Huda Beauty", category : "beauty",src : "assets/IMAGES/Beauty/Eyeshadow.png",off: 0, price : 699, des : "lorem1234"},
-    {name : "Lip Oil", companyName : "elf", category : "beauty",src : "assets/IMAGES/Beauty/Lip oil.png",off: 0, price: 600, des : "lorem1234"},
-    {name : "Lip Gloss", companyName : "elf", category : "beauty",src : "assets/IMAGES/Beauty/Lip gloss.png",off: 0, price : 500, des : "lorem1234"},
-    {name : "Makeup Brush Set", companyName : "Huda Beauty", category : "beauty",src : "assets/IMAGES/Beauty/Makeup brush set.png",off: 50, price : 500, des : "lorem1234"},
-    {name : "Retinol Serum", companyName : "CeraVe", category : "beauty",src : "assets/IMAGES/Beauty/Retinol serum.png",off: 0, price : 500, des : "lorem1234"},
-    {name : "Vitamin-C Super Serum", companyName : "Saeed Ghani", category : "beauty",src : "assets/IMAGES/Beauty/Vitamin C super serum.png",off: 0, price : 500, des : "lorem1234"},
-    
+    {name : "Face Wash", companyName : "ORGATRE", category : "beauty",src : "assets/IMAGES/Beauty/antiaging Face wash.png",off: 10,trending : false, price : 1299, des : ""},
+    {name : "Body Lotion", companyName : "eos", category : "beauty",src : "assets/IMAGES/Beauty/Body lotion.png",off: 0, trending : true, price : 449, des : ""},
+    {name : "Body Scrub", companyName : "Dove", category : "beauty",src : "assets/IMAGES/Beauty/Body Scrub.png",off: 10,trending : false,  price : 599, des : ""},
+    {name : "Eyeshadow", companyName : "Huda Beauty", category : "beauty",src : "assets/IMAGES/Beauty/Eyeshadow.png",off: 0,trending : false,  price : 699, des : ""},
+    {name : "Lip Oil", companyName : "elf", category : "beauty",src : "assets/IMAGES/Beauty/Lip oil.png",off: 0,trending : false,  price: 600, des : ""},
+    {name : "Lip Gloss", companyName : "elf", category : "beauty",src : "assets/IMAGES/Beauty/Lip gloss.png",off: 0,trending : false,  price : 500, des : ""},
+    {name : "Makeup Brush Set", companyName : "Huda Beauty", category : "beauty",src : "assets/IMAGES/Beauty/Makeup brush set.png",off: 50,trending : false,  price : 500, des : ""},
+    {name : "Retinol Serum", companyName : "CeraVe", category : "beauty",src : "assets/IMAGES/Beauty/Retinol serum.png",off: 0,trending : false,  price : 500, des : ""},
+    {name : "Vitamin-C Super Serum", companyName : "Saeed Ghani", category : "beauty",src : "assets/IMAGES/Beauty/Vitamin C super serum.png",off: 10,trending : false,  price : 500, des : ""},
+
+    {name : "Blind Spot Mirror", companyName : "", category : "automotive",src : "assets/IMAGES/Automotive/Blind Spot mirror.png",off: 20, trending : false,price : 1299, des : ""},
+    {name : "Car Care Kit", companyName : "", category : "automotive",src : "assets/IMAGES/Automotive/Car Care kit.png",off: 0, trending : true ,price : 6199, des : ""},
+    {name : "", companyName : "", category : "automotive",src : "assets/IMAGES/Automotive/",off: 0, trending :false ,price : 1299, des : ""},
 ];
